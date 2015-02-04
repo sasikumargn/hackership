a4905163145b2565a8d9dc71773a7666404e1963
SasiKumar G N <snarayanasamy@qwinixtech.com>
2015-02-04 16:47:13 +0530

    added layouts and views etc...

--- lib/carrierwave_processors/ffmpeg.rb
+++ lib/carrierwave_processors/ffmpeg.rb
@@ -9,16 +9,12 @@ module CarrierWave
     def encode_video(format)
       directory = File.dirname(current_path)
       tmpfile = File.join(directory, 'tmpfile')
-      # binding.pry
       File.rename(current_path, tmpfile)
-      # binding.pry
       file = ::FFMPEG::Movie.new(tmpfile)
       new_name = File.basename(current_path, '.*') + '.' + format.to_s
       current_extenstion = File.extname(current_path).gsub('.', '')
       encoded_file = File.join(directory, new_name)
-      # binding.pry
       file.transcode(encoded_file)
-
       # warning: magic! 
       # change format for uploaded file name and store file format
       # without this lines processed video files will remain in cache folder
